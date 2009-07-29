21b3c61cb44c76329f8dbc5b00fa06a40ea21391
mengruojun <mengruojun@89303d3e-7016-11de-9146-f165bea2e820>
2009-07-29 00:08:50 +0000

    重新建立工程

--- trunk/wptest/src/com/mrj/util/GlobalConstant.java
+++
@@ -1,7 +0,0 @@
-package com.mrj.util;
-
-public class GlobalConstant {
-	//public  static final String ExportFilePath ="C:\\new_zszq\\T0002\\export\\";
-	public  static final String ExportFilePath ="C:\\export_temp\\";
-	public  static final String ProjectPath ="D:\\mrj\\workspace\\wptest\\";
-}
