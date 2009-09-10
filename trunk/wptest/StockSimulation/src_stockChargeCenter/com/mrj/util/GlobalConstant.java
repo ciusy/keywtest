d79b192a6ef51a002763d2ab697037eea9916f76
mengruojun <mengruojun@89303d3e-7016-11de-9146-f165bea2e820>
2009-09-10 05:25:47 +0000

    remove some file to ignore list

--- trunk/wptest/StockSimulation/src_stockChargeCenter/com/mrj/util/GlobalConstant.java
+++
@@ -1,7 +0,0 @@
-package com.mrj.util;
-
-public class GlobalConstant {
-	public static final String ExportFilePath = "C://new_zszq//T0002//export//";
-	//public  static final String ExportFilePath ="C:/new_zszq/T0002/export_temp/";
-    public static final String ProjectPath = "C:/Users/ruojun/Workspaces/MyEclipse 7.0/StockSimulation";
-}
