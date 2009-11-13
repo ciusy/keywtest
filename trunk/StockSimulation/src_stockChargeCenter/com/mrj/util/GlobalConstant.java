77de643d57a10ccbb4f03559a7284f1964bb6f05
mengruojun <mengruojun@89303d3e-7016-11de-9146-f165bea2e820>
2009-11-13 02:03:26 +0000

    添加了可以作为netbeans工程的属性文件

--- trunk/StockSimulation/src_stockChargeCenter/com/mrj/util/GlobalConstant.java
+++ trunk/StockSimulation/src_stockChargeCenter/com/mrj/util/GlobalConstant.java
@@ -3,6 +3,6 @@ package com.mrj.util;
 public class GlobalConstant {
 	public static final String ExportFilePath = "C://new_zszq//T0002//export//";
 	//public static final String ProjectPath = "E:/DevelopeStudio/workspace/StockSimulation/";
-    public static final String ProjectPath = "C:/Users/ruojun/Workspaces/MyEclipse 7.0/StockSimulation/";
+    public static final String ProjectPath = "F:/mengrj/nbspace/StockSimulation/";
    
 }
