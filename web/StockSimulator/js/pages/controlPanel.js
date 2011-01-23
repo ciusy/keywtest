e050229285f4c4fa2fc7767a325d3e19187adddb
mengruojun <mengruojun@89303d3e-7016-11de-9146-f165bea2e820>
2011-01-23 13:29:40 +0000

    add web app support

---
+++ web/StockSimulator/js/pages/controlPanel.js
@@ -0,0 +1,11 @@
+YAHOO.namespace("YAHOO.jss.controlPanel");
+(function() {
+    var Y = YAHOO.util,
+        Dom = Y.Dom,
+        Event = Y.Event,
+        document = window.document;
+
+    YAHOO.jss.controlPanel = function(){
+
+    }
+})();
